bbe05105844f296cdb9edb907371fb0430abe880
anas zabili <anaszab78@gmail.com>
2021-02-10 21:24:32 +0100

    lesson 37 fin du petit projet

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
   <h1>Reaction timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="toggleEnd" />
+  <Result v-if="showResult" :reactionTime="score" />
 </template>
 
 <script>
 import Block from "./components/Block";
+import Result from "./components/Result";
 
 export default {
   name: "App",
-  components: { Block },
+  components: { Block, Result },
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResult: false,
     };
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
+      this.showResult = false;
+    },
+    toggleEnd(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
     },
   },
 };
@@ -33,5 +43,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time {{ reactionTime }} milliseconds</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["reactionTime"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.reactionTime < 250) {
+      this.rank = "Awsome";
+    } else if (this.reactionTime < 400) {
+      this.rank = "Good";
+    } else {
+      this.rank = "Come on hurry up";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
